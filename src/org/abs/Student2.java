3ec525f1fbd960e7ef5169dd7b491ca277a924f1
pugal05 <hrithikpugazh@gmail.com>
2022-05-20 21:05:20 +0530

    success

---
+++ src/org/abs/Student2.java
@@ -0,0 +1,15 @@
+package org.abs;
+
+public class Student2 {
+	
+	private void StudentDetails(String name) {
+		
+		System.out.println("student name"+name);
+
+	}
+	public static void main(String[] args) {
+		Student2 d = new Student2();
+		d.StudentDetails("mani");
+	}
+
+}
